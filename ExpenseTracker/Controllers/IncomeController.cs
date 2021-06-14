e37c97a5e93ca181ef923069c023e8c24df580d2
simeonov90 <vilensimeonov@abv.bg>
2021-06-14 23:33:07 +0300

    added income controller and post method for create income

---
+++ ExpenseTracker/Controllers/IncomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using ExpenseTracker.Data.Models.Dtos;
+using ExpenseTracker.Infrastructure.Claims;
+using ExpenseTracker.Repository.IRepository;
+using ExpenseTracker.Services.Contacts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.Controllers
+{
+    public class IncomeController : Controller
+    {
+        private readonly IIncomeRepository incomeRepository;
+        private readonly IIncomeService incomeService;
+        private readonly IMapper mapper;
+
+        public IncomeController(IIncomeRepository incomeRepository, IIncomeService incomeService, IMapper mapper)
+        {
+            this.incomeRepository = incomeRepository;
+            this.incomeService = incomeService;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateIncome([FromBody]IncomeCreateDto incomeCreateDto)
+        {
+            if (incomeCreateDto == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (!await this.incomeService.CreateIncome(incomeCreateDto, this.User.GetUserId()))
+            {
+                ModelState.AddModelError("", "Something went wrong with saving date.");
+                return StatusCode(500, ModelState);
+            }
+
+            return Json("Ok");
+        }
+    }
+}
