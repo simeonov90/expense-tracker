5048a8ed978be52249203fb658b9fdb1b9892ef6
simeonov90 <vilensimeonov@abv.bg>
2021-06-14 11:49:00 +0300

    added income repository

---
+++ ExpenseTracker/Repository/IRepository/IIncomeRepository.cs
@@ -0,0 +1,18 @@
+﻿using ExpenseTracker.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Repository.IRepository
+{
+    public interface IIncomeRepository
+    {
+        Task<ICollection<Income>> GetAllIncome();
+        Task<Income> GetIncome(int incomeId);
+        Task<bool> CreateIncome(Income income);
+        Task<bool> DeleteIncome(Income income);
+        Task<bool> IncomeExists(int incomeId);
+        Task<bool> Save();
+    }
+}
---
+++ ExpenseTracker/Repository/IncomeRepository.cs
@@ -0,0 +1,53 @@
+﻿using ExpenseTracker.Data;
+using ExpenseTracker.Data.Models;
+using ExpenseTracker.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Repository
+{
+    public class IncomeRepository : IIncomeRepository
+    {
+        private readonly ApplicationDbContext db;
+
+        public IncomeRepository(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<bool> CreateIncome(Income income)
+        {
+            await this.db.Incomes.AddAsync(income);
+            return await Save();
+        }
+
+        public async Task<bool> DeleteIncome(Income income)
+        {
+            this.db.Incomes.Remove(income);
+            return await Save();
+        }
+
+        public Task<ICollection<Income>> GetAllIncome()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Income> GetIncome(int incomeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IncomeExists(int incomeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
